a5127b012fa19c125b696edf63ce801a9659aebd
Phuong <trantuanphuong19941994@gmail.com>
2016-01-28 22:07:08 +0700

    Edit,Delete,Show,List articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,11 @@
 class ArticlesController < ApplicationController
-	
+
+	def index
+	  @articles = Article.all
+	end
+
 	def new
-		@article = Article.new	
+	  @article = Article.new	
 
 	end
 
@@ -19,9 +23,30 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
-	def index
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+		  flash[:notice] = "Article was successfully edited"
+		  redirect_to article_path(@article)
+		else
+		  render 'edit'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+	    @article.destroy
+		redirect_to article_path(@article)
+		flash[:notice] = "Article was successfuly deleted"
+		redirect_to article_path(@article)
 
 	end
+
+
 
 	private
 		def article_params
