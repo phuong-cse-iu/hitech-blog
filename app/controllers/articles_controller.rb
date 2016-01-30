5150744aac3e19524ed4757c6d056dc70114a0da
Phuong <trantuanphuong19941994@gmail.com>
2016-01-30 13:38:41 +0700

    Association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,13 +10,14 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(article_params)
-		if @article.save
-			flash[:success] = "Article was successfully created"
-			redirect_to article_path(@article)
-		else
-			render 'new'
-		end
+	  @article = Article.new(article_params)
+	  @article.user = User.first
+	  if @article.save
+		flash[:success] = "Article was successfully created"
+		redirect_to article_path(@article)
+	  else
+		render 'new'
+	  end
 	end
 
 	def show
