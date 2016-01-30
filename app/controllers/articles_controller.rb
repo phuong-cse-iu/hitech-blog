b09b7040262b8aaebef3b3cae906eb9747d004ee
Phuong <trantuanphuong19941994@gmail.com>
2016-01-30 13:40:23 +0700

    Association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,14 +10,13 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-	  @article = Article.new(article_params)
-	  @article.user = User.first
-	  if @article.save
-		flash[:success] = "Article was successfully created"
-		redirect_to article_path(@article)
-	  else
-		render 'new'
-	  end
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:success] = "Article was successfully created"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
 	end
 
 	def show
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130055258) do
+ActiveRecord::Schema.define(version: 20160130051330) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
