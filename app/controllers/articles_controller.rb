da2057c32a59418ef3d93e1c8ce02ba5d543d679
Phuong <trantuanphuong19941994@gmail.com>
2016-02-01 20:58:49 +0700

    Add gravatar, user's profile and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
-		@article.user = User.last
+		@article.user = User.find(5)
 		if @article.save
 			flash[:success] = "Article was successfully created"
 			redirect_to article_path(@article)
