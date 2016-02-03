514425f56021ca91b5db026207822b6c75ec2e0f
Phuong <trantuanphuong19941994@gmail.com>
2016-02-03 18:45:06 +0700

    Make app ready for deploying

--- db/migrate/20160203113504_create_articles.rb
+++ db/migrate/20160203113504_create_articles.rb
@@ -1,10 +1,10 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-    	t.string :title
-    	t.text :description
-    	t.datetime :created_at
-    	t.datetime :updated_at
+      t.string :title
+      t.text :description
+      t.timestamps
+      t.integer :user_id
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130140233) do
+ActiveRecord::Schema.define(version: 20160203113504) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
