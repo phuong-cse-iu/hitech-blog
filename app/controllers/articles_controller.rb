f315ff1b68584d80e442ddf01bd4acc4107d39e1
Phuong <trantuanphuong19941994@gmail.com>
2016-02-04 20:27:23 +0700

    Set admin and add admin's functionalities

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,24 +3,24 @@ class ArticlesController < ApplicationController
   before_action :require_user, except: [:index, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   def index
-	@articles = Article.paginate(page: params[:page], per_page: 5)
+	  @articles = Article.paginate(page: params[:page], per_page: 5)
 	  
   end
 
   def new
-	@article = Article.new	
+	  @article = Article.new	
 
   end
 
   def create
-	@article = Article.new(article_params)
-	@article.user = current_user
-	if @article.save
-	  flash[:success] = "Article was successfully created"
-	  redirect_to article_path(@article)
-	else
-	  render 'new'
-	end
+	  @article = Article.new(article_params)
+	  @article.user = current_user
+	  if @article.save
+	    flash[:success] = "Article was successfully created"
+	    redirect_to article_path(@article)
+	  else
+	    render 'new'
+	  end
   end
 
   def show
@@ -30,18 +30,18 @@ class ArticlesController < ApplicationController
   end
 
   def update
-	if @article.update(article_params)
-	  flash[:success] = "Article was successfully edited"
-	  redirect_to article_path(@article)
-	else
-	  render 'edit'
+	  if @article.update(article_params)
+	    flash[:success] = "Article was successfully edited"
+	    redirect_to article_path(@article)
+	  else
+	    render 'edit'
     end
   end
 
   def destroy
-	@article.destroy
-	flash[:danger] = "Article was successfuly deleted"
-	redirect_to articles_path
+	  @article.destroy
+	  flash[:danger] = "Article was successfuly deleted"
+	  redirect_to articles_path
 
   end
 
@@ -55,7 +55,7 @@ class ArticlesController < ApplicationController
 	end
 
     def require_same_user
-      if current_user!= @article.user
+      if current_user!= @article.user and !current_user.admin?
       	flash[:danger] = "You can only edit or delete your own articles"
       	redirect_to root_path
       end
